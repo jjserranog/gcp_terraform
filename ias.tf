26a29f99954da44df86ef4d613c055bdc9375aff
jjserranog <jjserranog@gmail.com>
2023-03-06 15:25:43 +0100

    deleteion yes

--- ias.tf
+++ ias.tf
@@ -1,10 +1,11 @@
 #Create a single Compute Engine instance default
 resource "google_compute_instance" "default" {
   name         = "duaassvgsrpro01"
+  deletion_protection = " true"
   machine_type = "e2-highmem-8"
   zone         = "eu-west1-a"
   tags         = ["un=air"]
-
+  
   boot_disk {
     initialize_params {
       image = "windows-cloud/windows-2022"
